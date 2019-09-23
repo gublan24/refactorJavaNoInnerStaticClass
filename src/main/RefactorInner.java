5e50a6506f7c762e870f38aaaa7ae30ec6340fa6
Abdulaziz <gublan24@gmail.com>
2019-09-22 22:19:47 -0400

    write only files with static

--- src/main/RefactorInner.java
+++ src/main/RefactorInner.java
@@ -12,54 +12,46 @@ import java.util.ArrayList;
 public class RefactorInner {
 
 	public static void main(String[] args) {
-		
+
 		String dir = new File("").getAbsolutePath();
-		String outDir = dir+"/refactor_InnerStaticClass_output/";
+		String outDir = dir + "/refactor_InnerStaticClass_output/";
 		if (args.length > 0)
 			dir = args[0];
 		if (args.length > 1)
 			outDir = args[1];
 
-		
 		BufferedWriter logFile = null;
 		try {
-			logFile = new BufferedWriter(new FileWriter("log.txt",true));
-			logFile.append("---------------------------------------------------------"+"\n");
-			
+			logFile = new BufferedWriter(new FileWriter("log.txt", true));
+			logFile.append("---------------------------------------------------------" + "\n");
+
 			ArrayList<String> fileNames = getAllFilesInDirectory(dir);
 			if (fileNames.size() > 0) {
 				for (String file : fileNames) {
 					writeFilesWithCommentedInnerStaticClasses(file, outDir, logFile);
-
 				}
 			}
-			
+
 			logFile.close();
-			
-			
-			
-			
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
 
 	}
 
 	private static void writeFilesWithCommentedInnerStaticClasses(String file, String outDir, BufferedWriter logFile) {
-		
-		
-		try {
-			
+
+		try 
+		{
 			BufferedReader buffReader = new BufferedReader(new FileReader(file));
 			File inputFile = new File(file);
 			File directory = new File(outDir);
-	    if (! directory.exists()){
-	        directory.mkdir();
-	    }			
+			if (!directory.exists()) 
+			{
+				directory.mkdir();
+			}
 			String line = "";
 			boolean ecounterStatic = false;
 			int open_bracket_count = 0;
@@ -67,75 +59,85 @@ public class RefactorInner {
 
 			try 
 			{
-				String outputFullFileName = outDir+inputFile.getName();
-				BufferedWriter javaFile = new BufferedWriter(new FileWriter(outputFullFileName));//TODO BufferedWriter writeFilesHavingStatic
+				String outputFullFileName = outDir + inputFile.getName();
 				System.out.println(outputFullFileName);
-	
 
-				while((line=buffReader.readLine()) != null)
+				String newJavaFilecontent = "";
+				boolean toWriteNewJavaFile = false;
+				
+				while ((line = buffReader.readLine()) != null) 
 				{
-					if(line.contains("static class"))
+					if (line.contains("static class")) 
 					{
 						ecounterStatic = true;
-						int numberofOpenBracket= countOccurrence(line, "{");
-						int numberofCloseBracket= countOccurrence(line, "}");
+						int numberofOpenBracket = countOccurrence(line, "{");
+						int numberofCloseBracket = countOccurrence(line, "}");
 						open_bracket_count += numberofOpenBracket;
 						close_bracket_count += numberofCloseBracket;
-						
-						//System.out.println("//"+line);
-						javaFile.write("// START_OF_STATIC_CLASS \n");
-						javaFile.write("//"+line+"\n");
-						logFile.append(""+file + ">>"+outputFullFileName +"\n");
-				
+						// System.out.println("//"+line);
+						newJavaFilecontent += "// START_OF_STATIC_CLASS \n";
+						newJavaFilecontent += "//"+ line + "\n";
+						logFile.append("" + file + "\n \t\t\t>>\t\t\t" + outputFullFileName + "\n");
+						logFile.append(" ---- ---- ---- "+"\n");
 
 						
-					} else if (ecounterStatic)
+						toWriteNewJavaFile = true;
+					} 
+					else if (ecounterStatic) 
 					{
-						int numberofOpenBracket= countOccurrence(line, "{");
-						int numberofCloseBracket= countOccurrence(line, "}");
+						int numberofOpenBracket = countOccurrence(line, "{");
+						int numberofCloseBracket = countOccurrence(line, "}");
 						open_bracket_count += numberofOpenBracket;
 						close_bracket_count += numberofCloseBracket;
-					//	System.out.println("//"+line);
-						javaFile.write("//"+line+"\n");
+						newJavaFilecontent += "//"+ line + "\n";
 
-
-						if(close_bracket_count == open_bracket_count)
-						{ //reset 
+						if (close_bracket_count == open_bracket_count) { // reset
 							ecounterStatic = false;
 							open_bracket_count = 0;
 							close_bracket_count = 0;
-							//System.out.println("//"+line);
-							javaFile.write("//"+line+"\n");
-							javaFile.write("// END_OF_STATIC_CLASS \n");
-
-
+							newJavaFilecontent += "//"+ line + "\n";
+							newJavaFilecontent += "// END_OF_STATIC_CLASS \n";
 						}
-					}
-					else 
+					} 
+					else
 					{
-//						System.out.println(line); // do nothing 
-						javaFile.write(line+"\n");
-
+						newJavaFilecontent += line + "\n";
 					}
 				}
 				
-				javaFile.flush();
-				javaFile.close();
+				if(toWriteNewJavaFile)
+				{
+					
+					BufferedWriter newJavaFileBufferedWriter = new BufferedWriter(new FileWriter(outputFullFileName));
+					writeToFile(newJavaFilecontent, newJavaFileBufferedWriter);
+				
+					
+				}
+				
+				
 				logFile.flush();
-		
-			
-			} 
-			
-			catch (IOException e) {
+			}
+
+			catch (IOException e) 
+			{
 				e.printStackTrace();
 			}
-			
-			
-		} catch (FileNotFoundException  e) {
+
+		} 
+		catch (FileNotFoundException e) {
 			System.out.println("-----------------------------------------");
 			e.printStackTrace();
 		}
 
+	}
+
+	private static void writeToFile(String fileContent, BufferedWriter bufferedWriter) throws IOException {
+		if(bufferedWriter != null)
+		{
+			bufferedWriter.write(fileContent );
+			bufferedWriter.flush();
+			bufferedWriter.close();
+		}
 	}
 
 	private static ArrayList<String> getAllFilesInDirectory(String dir) {
@@ -147,8 +149,8 @@ public class RefactorInner {
 		for (File file : allSubFiles) {
 			if (file.isDirectory()) {
 				files.addAll(getAllFilesInDirectory(file.getAbsolutePath()));
-
-			} else {
+			} 
+			else {
 				files.add(file.getAbsolutePath());
 
 			}
@@ -156,17 +158,15 @@ public class RefactorInner {
 
 		return files;
 	}
-	
+
 	public static int countOccurrence(String source, String sentence) {
 		int occurrences = 0;
-
-    if (source.contains(sentence)) {
-        int withSentenceLength    = source.length();
-        int withoutSentenceLength = source.replace(sentence, "").length();
-        occurrences = (withSentenceLength - withoutSentenceLength) / sentence.length();
-    }
-
-    return occurrences;
-}
+		if (source.contains(sentence)) {
+			int withSentenceLength = source.length();
+			int withoutSentenceLength = source.replace(sentence, "").length();
+			occurrences = (withSentenceLength - withoutSentenceLength) / sentence.length();
+		}
+		return occurrences;
+	}
 
 }
